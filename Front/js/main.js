8769baa31751aaca6e652a430339757392700d96
Fabien Massucco <f9bm9s@gmail.com>
2020-10-15 15:57:55 +0200

    Front waiting for Back...

--- Front/js/main.js
+++ Front/js/main.js
@@ -1,48 +1,139 @@
-const xxx = document.querySelector("#xxx");
-const urlPath = "http://xyz";
-let urlSlug = "";
-const urlQuery = "?";
-let urlApi = urlPath + urlSlug + urlQuery;
+import * as tJs from "./topic.js";
+const docIdTopicsList = document.querySelector("#topicsList");
+const docIdCreateTopicForm = document.querySelector("#createTopicForm");
+export const fetchesParams = [
+  { method: "GET",
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    mode: "cors",
+    cache: "default"},
+  { method: "POST",
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: null,
+    mode: "cors",
+    cache: "default"},
+  { method: "PATCH",
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: null,
+    mode: "cors",
+    cache: "default"}
+];
 
-selArea.addEventListener("change", () => {
-  changeMapSrc(selArea.options[selArea.selectedIndex].text);
-  urlSlug = selArea.value;
-  urlApi = urlPath + urlSlug + urlQuery;
-  tableBody.innerHTML = tableBody.oldHTML;
-  loadCountries(urlApi);
-  selArea.blur();
+window.addEventListener("DOMContentLoaded", (event) => {
+  docIdCreateTopicForm.addEventListener(
+    "submit", (e) => {
+      e.preventDefault(); // Cancel submit default behavior
+      submitBehavior(docIdCreateTopicForm);    
+    },
+    false
+  );
 });
+/* document.onload = orderAndServe(setUrlEndpoint("/topics", "?size=10"), fetchesParams[0], list); */
 
-document.onload = loadTopics(urlApi);
+// Create form submit behavior defining api.fetches specific params
+export function submitBehavior(form) {
+  const submitTimestamp = Date.now();
+  const urlSlug = "/topics/";
+  const newId = submitTimestamp+"_"+form.querySelector("input:first-of-type").value;
 
-function loadTopics(url) {
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((jsonResp) => list(jsonResp))
-    .catch((error) => console.log("Erreur : " + error));
+  const data = new FormData(form);
+  data.append("postId", newId);
+  data.append("submitDateTime", submitTimestamp);
+  const jsonData = JSON.stringify(Object.fromEntries(data));
+  console.log(jsonData);
+  
+  const url = setUrlEndpoint(urlSlug, "");
+
+  const fetchParams = (form.getAttribute("method") == "post")? 
+    fetchesParams[1].body = jsonData:
+    fetchesParams[2].body = jsonData;
+  console.log(fetchParams);
+  
+  console.log((form.getAttribute("data-slug").includes("topic")));
+  const callback = (form.getAttribute("data-slug").includes("topic"))?
+    newTopicSuccess:
+    tJs.newReplySuccess;
+
+  /* orderAndServe(url, fetchParams, callback); */
+  callback(jsonData); // for test purpose until backend release
 }
 
-function list(topics) {
+export function orderAndServe(url, params, callback) {
+  fetch(url, params)
+    .then((resp) => {
+      if (response.status !== 200) {
+        alert(
+          "La réponse du serveur ne nous comble point... : \n" + response.status
+        );
+        return;
+      }
+      (["POST", "PUT", "PATCH"].includes(params.method))? callback(params.body): callback(resp);
+    })
+    .catch((error) =>
+      alert("Fichtre! Une erreur nous casse les fetchs : \n" + error)
+    );
+}
+
+function newTopicSuccess(jsonTopic) {
+  window.location.href = "/topic.html";
+  sessionStorage.setItem('newTopicData', jsonTopic);
+  sessionStorage.setItem('currentNewTopic', JSON.parse(jsonTopic).postId)
+}
+
+export function list(topics) {
   console.log(topics);
   topics.forEach((topic) => {
     //
   });
 }
 
-function notNullCheck(el) {
+export function makeTopicClickable() {
+  document.getElementById("#").addEventListener("click", function () {
+    //;
+  });
+}
+
+export function setUrlEndpoint(slug, params) {
+  const urlPath = "http://localhost:8089";
+  const urlEndpoint = urlPath + slug + params;
+  console.log(urlEndpoint);
+  return urlEndpoint;
+}
+
+export function notNullCheck(el) {
   if (el != null) {
-    return el;
-  } else return "N/A";
+    return true;
+  } else return false;
 }
 
-function numFormat(x) {
-  return new Intl.NumberFormat("en-GB", { maximumFractionDigits: 3 }).format(x);
+export function createChildWithIdAndValueFromArray(parent, childTag, array, i) {
+  if (array[i][0] != "postId") {
+    const child = createNode(childTag);
+    child.setAttribute("id", array[i][0])
+    if (array[i][0] == "submitDateTime") {
+      child.innerHTML = formatDate(Date(array[i][1]));
+    }  else {
+      child.innerHTML = array[i][1];
+    }
+    append(parent, child);
+  }
 }
 
-function append(parent, el) {
+export function append(parent, el) {
   return parent.appendChild(el);
 }
 
-function createNode(el) {
+export function createNode(el) {
   return document.createElement(el);
+}
+
+function formatDate(d) {
+  const date = new Date(d)
+  const dd = date.getDate(); 
+  const mm = date.getMonth()+1;
+  const yyyy = date.getFullYear();
+  const HH = date.getHours();     
+  const MM = date.getMinutes()
+  if(dd<10){dd='0'+dd} 
+  if(mm<10){mm='0'+mm};
+  return d = dd+'/'+mm+'/'+yyyy+' '+HH+':'+MM
 }
---
+++ Front/js/topic.js
@@ -0,0 +1,73 @@
+import * as mJs from "./main.js";
+const docIdReplyTopicForm = document.querySelector("#replyTopicForm");
+const docIdTopicInfos = document.querySelector("#topicInfos");
+const docIdTopicText = document.querySelector("#topicText");
+const docIdRepliesList = document.querySelector("#repliesList");
+const docIdReplyTopicButton = document.querySelector("#replyTopicButton");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const newTopicData = sessionStorage.getItem('newTopicData');
+  const currentNewTopic = sessionStorage.getItem('currentNewTopic');
+  const topicToLoad = sessionStorage.getItem('topicToLoad');
+  const topicData = newTopicData;//(mJs.notNullCheck(newTopicData))? newTopicData : loadTopic(topicToLoad);
+
+  displayTopic(topicData);
+  
+  
+/*   (mJs.notNullCheck(newTopicData))? 
+    sessionStorage.removeItem('newTopicData'):
+    sessionStorage.removeItem('topicToLoad'); */
+
+/*   docIdReplyTopicButton.addEventListener(
+    "click", (e) => {
+      docIdReplyTopicForm.setAttribute("hidden", false);
+    },
+    false
+  ); */
+  docIdReplyTopicForm.addEventListener(
+    "submit", (e) => {
+      e.preventDefault();
+      mJs.submitBehavior(docIdReplyTopicForm);    
+    },
+    false
+  );
+});
+
+function loadTopic (topicId){
+  //mJs.orderAndServe(mJs.setUrlEndpoint("/topics/", topicId), mJs.fetchesParams[0], displayTopic);
+}
+
+function displayTopic(jsonTopic) {
+  const topic = Object.entries(JSON.parse(jsonTopic));  
+  for (let i = 0; i < topic.length; i++) {
+    if (topic[i][0] == "topicMsg") {
+      docIdTopicText.innerHTML = topic[i][1];
+    } else if (topic[i][0] == "repliesList") {
+      topic[i][0].forEach(reply => { 
+        const li = mJs.createNode("li");
+        mJs.append(docIdRepliesList, li);
+        displayReply (reply);        
+      });
+    } else {
+      mJs.createChildWithIdAndValueFromArray(docIdTopicInfos, "span", topic, i);
+    }
+  }
+}
+
+function displayNewReply (jsonReply) {
+  const reply = Object.entries(JSON.parse(jsonReply));
+  displayReply (reply);
+}
+
+function displayReply (reply) {  
+  const li = mJs.createNode("li");
+  mJs.append(docIdRepliesList, li);
+  for (let i = 0; i < reply.length; i++) {
+    mJs.createChildWithIdAndValueFromArray(li, "span", reply, i);
+  }
+}
+
+export function newReplySuccess(jsonReply) {
+  displayNewReply(jsonReply);
+  docIdReplyTopicForm.reset();
+}
